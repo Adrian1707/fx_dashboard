c0c13e3be5901f4a49065f3426b1cc2d47c3ebff
Adrian Booth <adrian.booth17@gmail.com>
2023-08-12 11:59:39 +0700

    Use sample data to return currency rates to prevent api rate limits

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,16 +1,59 @@
 import * as React from 'react';
-const { useState } = React;
+const { useState, useEffect } = React;
 import { FxRate } from "./FxRate";
+import exampleData from './example_data.json';
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 export function Dashboard() {
+  const [exchangeData, setExchangeData] = useState(null);
+  const FX_API_KEY = process.env.FX_API_KEY
+  const TIME_SERIES_URL = "https://marketdata.tradermade.com/api/v1/timeseries"
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Use this for testing data to prevent hitting API limits
+        const data = exampleData
+
+        // const response = await fetch(
+        //   `${TIME_SERIES_URL}?currency=GBPTHB&api_key=${FX_API_KEY}&start_date=2023-07-01&end_date=2023-08-10&format=records`
+        // );
+
+        // if (!response.ok) {
+        //   throw new Error('Network response was not ok');
+        // }
+
+        // const data = await response.json(); // Parse the response
+        return data;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        return null;
+      }
+    };
+
+    const fetchDataAndProcess = async () => {
+      const data = await fetchData();
+      if (data) {
+        setExchangeData(data);
+      }
+    };
+
+    fetchDataAndProcess();
+}, []);
 
   return(
     <div>
-      <div className="grid gap-6 lg:grid-cols-3">
-        <FxRate />
-        <FxRate />
-        <FxRate />
-      </div>
+    { exchangeData &&
+      (
+        <div className="grid gap-6 lg:grid-cols-3">
+          <FxRate data={exchangeData} />
+          <FxRate data={exchangeData} />
+          <FxRate data={exchangeData} />
+        </div>
+      )
+    }
     </div>
   )
 }
--- src/FxRate.js
+++ src/FxRate.js
@@ -2,8 +2,17 @@ import * as React from 'react';
 const { useState } = React;
 import { LineChart, Line } from 'recharts';
 
-export function FxRate() {
-  const [rates, setRates] = useState([1.25, 1.27, 1.32, 1.31, 1.28])
+export function FxRate(data) {
+  const formatCurrencyPair = () => {
+    return `${data.data.base_currency}/${data.data.quote_currency}`
+  }
+
+  const collectRates = () => {
+    return data.data.quotes.map((rate) => rate.close)
+  }
+
+  const [currencyPair, setCurrencyPair] = useState(formatCurrencyPair)
+  const [rates, setRates] = useState(collectRates)
 
   return(
     <div className='zoom relative overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20 bg-[50%]'>
@@ -11,10 +20,10 @@ export function FxRate() {
       className="transition duration-300 ease-linear"/>
       <a href="#!">
          <div
-           class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed bg-[hsla(0,0%,0%,0.3)]">
-           <div class="flex h-full items-end justify-start">
-             <h5 class="m-6 text-lg font-bold text-white">
-               GBP/USD
+           className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed bg-[hsla(0,0%,0%,0.3)]">
+           <div className="flex h-full items-end justify-start">
+             <h5 className="m-6 text-lg font-bold text-white">
+               {currencyPair}
              </h5>
            </div>
          </div>
